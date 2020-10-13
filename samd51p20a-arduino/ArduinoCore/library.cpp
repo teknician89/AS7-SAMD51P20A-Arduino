35fcdf09151b5b8ddcdf8b78aff27bf78c2c11aa
Daniel Harris <teknician89@gmail.com>
2020-10-12 23:16:57 -0600

    Adding project files.

---
+++ samd51p20a-arduino/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 10/9/2020 1:06:49 PM
+ * Author : Daniel
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
